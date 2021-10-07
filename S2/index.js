ee03bd488e86d1e70dc847844c1d42b523e61c0f
BenoitDossoine <benoit.dossoine@student.ehb.be>
2021-10-07 10:48:41 +0200

    List of pokemons made
    Succesfully fetching list of pokemons and filling the list in dynamically

--- S2/index.js
+++ S2/index.js
@@ -4,8 +4,34 @@ function fetchPokemon() {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
         .then(response => response.json())
         .then(data => {
-            console.log(data.results);
+            data.results.forEach(value => {
+                fetch(value.url)
+                    .then(response => response.json())
+                    .then(data => {
+                        // console.log(data);
+                        // console.log(data.name);
+                        let pokemonType = [];
+                        let typeDiv = "";
+                        data.types.forEach(type => {
+                            pokemonType.push(type.type.name);
+                        });
+                        pokemonType.forEach(type => {
+                            typeDiv += `<p class="${type}">${type}</p>`;
+                        });
+                        let pokemonTile = `<div class="pokemonItem">
+                        <img class="pokemonImg" src="${data.sprites.front_default}" alt="${data.name}">
+                        <p class="pokemonNr">${data.id}</p>
+                        <p class="pokemonName">${data.name}</p>
+                        <div class="pokemonType">
+                            ${typeDiv}  
+                        </div>
+                    </div>`;
+                        document.getElementById("pokemonList").innerHTML += pokemonTile;
+                    });
+            });
         });
 }
+
+
 
 fetchPokemon();
