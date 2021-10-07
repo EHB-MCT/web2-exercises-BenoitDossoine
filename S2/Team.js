3f9139157e3af1ec19007391c661a83018daa2c1
BenoitDossoine <benoit.dossoine@student.ehb.be>
2021-10-07 12:28:32 +0200

    Removed DOM handling in class
    DOM handling in class was advised against, so moved it to index.js

--- S2/Team.js
+++ S2/Team.js
@@ -2,14 +2,14 @@
 
 class Team {
     constructor() {
-        this.teamname = 'Example teamname';
-        this.trainer = 'Example trainername';
-        this.roster = ["Bulbasaur","Bulbasaur","Bulbasaur"];
+        this.teamname = 'Benoit\'s team';
+        this.trainer = 'Benoit';
+        this.roster = [];
     }
     describeTeam(){
         let description = `This is team ${this.teamname} by trainer ${this.trainer}:`;
         let team = [...this.roster];
-        document.getElementById("team").innerHTML = `<div>${description}<\div>
+        return `<div>${description}<\div>
         <div>${team}<\div>`;
     }
 
