51d89a1c91ce59b04ce318e8d7d7772739ffc98d
BenoitDossoine <benoit.dossoine@student.ehb.be>
2021-10-21 09:07:26 +0200

    WIP Navigation
    Added navigation buttons, but not all working yet.

--- LEVELS/index.js
+++ LEVELS/index.js
@@ -1,6 +1,10 @@
 "use strict";
 let users = [];
 window.onload = function () {
+    let navigation = document.getElementById("navigation");
+    navigation.addEventListener("click", (event) => {
+        displayPage(event.target);
+    })
     let loginForm = document.getElementById("loginForm");
     loginForm.addEventListener("submit", (event) => {
         event.preventDefault();
@@ -12,6 +16,23 @@ window.onload = function () {
         createStudent(userName, fullName, email, course);
         console.log(users);
     })
+}
+
+function displayPage(target) {
+    if (target.id == "loginNav") {
+        document.getElementById("loginSection").style.display = "flex";
+        document.getElementById("profileSection").style.display = "none";
+        document.getElementById("leaderboardSection").style.display = "none";
+    } else if (target.id == "profileNav") {
+        document.getElementById("loginSection").style.display = "none";
+        document.getElementById("profileSection").style.display = "flex";
+        document.getElementById("leaderboardSection").style.display = "none";
+    } else if (target.id == "leaderboardNav") {
+        document.getElementById("loginSection").style.display = "none";
+        document.getElementById("profileSection").style.display = "none";
+        document.getElementById("leaderboardSection").style.display = "flex";
+
+    }
 }
 
 function createStudent(userName, fullName, email, course) {
